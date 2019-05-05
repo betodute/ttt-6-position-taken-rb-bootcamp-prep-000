7578ff9061a84651fd8f731accddb66719ed9b28
Beto Dute <betodute@gmail.com>
2019-05-05 23:57:45 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 def position_taken?(board, index)
  if board[index] == nil
    false
- else !((board[index] == "") || (board[index] == " "))
-end
+ else !(board[index] == "") || (board[index] == " ")
+ end
 end
